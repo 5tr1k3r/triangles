ec45b2011cb606bbddbe0fc1f6cee91d6f66176e
Jabarton <noblemonitor@gmail.com>
2022-04-30 21:05:52 +0500

    implement cached views, they are created upon first demand. custom puzzle playviews are not cached

--- views/view_manager.py
+++ views/view_manager.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict
 
 import arcade
 
@@ -10,25 +10,40 @@ from views.solve_view import SolveView
 class ViewManager:
     def __init__(self):
         self.window: Optional[arcade.Window] = None
-        self.menu = MenuView()
-        self.solver = SolveView()
+        self.cached_views: Dict[str, arcade.View] = {}
 
     def confirm_window_exists(self):
         if self.window is None:
             self.window = arcade.get_window()
 
+    def get_view_from_cache(self, view_type: type) -> arcade.View:
+        name = view_type.__name__
+        if name not in self.cached_views:
+            self.cached_views[name] = view_type()
+
+        return self.cached_views[name]
+
     def show_menu_view(self):
         self.confirm_window_exists()
-        self.window.show_view(self.menu)
+        menu_view = self.get_view_from_cache(MenuView)
+        self.window.show_view(menu_view)
 
     def show_play_view(self):
         self.confirm_window_exists()
-        self.window.show_view(PlayView())
+        play_view = self.get_view_from_cache(PlayView)
+        self.window.show_view(play_view)
 
     def show_play_view_with_custom_puzzle(self, puzzle_code: str):
         self.confirm_window_exists()
+
+        # Not caching this because making it cacheable
+        # would require a rework of the cache system,
+        # e.g. key would need to contain the puzzle code/args.
+        # Don't need to cache it that much anyway
+        # because paths aren't generated and it works quickly regardless.
         self.window.show_view(PlayView(puzzle_code))
 
     def show_solve_view(self):
         self.confirm_window_exists()
-        self.window.show_view(self.solver)
+        solve_view = self.get_view_from_cache(SolveView)
+        self.window.show_view(solve_view)
